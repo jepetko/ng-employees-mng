3e9d724676b552b02d535ed54487024547734ea4
katarina <golbang.k@gmail.com>
2015-01-05 20:36:29 +0100

    test for departments

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe Department, :type => :model do
   before(:each) do
     @dep = FactoryGirl.create(:department)
+    @invalid_dep = Department.new
   end
 
   it 'should have a name' do
     expect(@dep).to respond_to(:name)
+  end
+
+  it 'is invalid when name is empty' do
+    expect(@invalid_dep).to be_invalid
+    expect(@invalid_dep.errors[:name]).to include('can\'t be blank')
   end
 end
