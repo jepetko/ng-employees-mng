08e82194f601a6bf533a83f89b236eb2e98e4935
katarina <golbang.k@gmail.com>
2015-01-05 21:25:19 +0100

    employees controller routing test

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
 
   resources :departments, only: [:index]
-  resources :employees
+  resources :employees, except: [:edit]
 end
--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -4,6 +4,32 @@ describe 'EmployeesController' do
   render_views
 
   before(:each) do
-
+    department = FactoryGirl.create :department
+    @employees = []
+    10.times do
+      @employees << FactoryGirl.create(:random_employee, department: department)
+    end
   end
+
+  describe 'routing' do
+    it 'routes to #index' do
+      expect({ get: 'employees'}).to route_to( {controller: 'employees', action: 'index'})
+    end
+    it 'routes to #show' do
+      expect({ get: "employees/#{@employees.first.id}"}).to route_to({controller: 'employees', action: 'show', id: @employees.first.id.to_s})
+    end
+    it 'routes to #create' do
+      expect({ post: 'employees'}).to route_to({controller: 'employees', action: 'create'})
+    end
+    it 'routes to #update' do
+      expect({put: "employees/#{@employees.first.id}"}).to route_to({controller: 'employees', action: 'update', id: @employees.first.id.to_s})
+    end
+    it 'routes to #destroy' do
+      expect({delete: "employees/#{@employees.first.id}"}).to route_to({controller: 'employees', action: 'destroy', id: @employees.first.id.to_s})
+    end
+    it 'doesn\'t route to #edit' do
+      expect({get: "employees/#{@employees.first.id}/edit"}).not_to be_routable
+    end
+  end
+
 end
